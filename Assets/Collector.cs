e17dade9f7fbe1fb95d92186d68e3e74cde09465
Leonid Sinyakov <redeyerulk@gmail.com>
2016-01-30 01:24:00 +0300

    initial commit

---
+++ Assets/Collector.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Collector : MonoBehaviour {
+
+    static public Collector s_myCollector;
+    POI target = null;
+    float distanceToPOI;
+
+    [SerializeField]
+    Transform homeEmitter;
+
+    NavMeshAgent agent;
+    // Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        s_myCollector = this;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+	    if(target != null )
+        {
+            agent.SetDestination(target.transform.position);
+            if (!agent.pathPending)
+            {
+                float dist = Vector3.Distance(transform.position, target.transform.position);
+                if (dist <= 1.5f)
+                {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                    {
+                        Destroy(target.gameObject);
+                        target = null;
+                    }
+                }
+            }
+        }
+        else
+        {
+            agent.SetDestination(homeEmitter.position);
+
+        }
+	}
+
+    public void addPOI(POI poi)
+    {
+        float dist = Vector3.Distance(poi.transform.position, transform.position);
+        if (target == null)
+        {
+            target = poi;
+            distanceToPOI = dist;
+        }
+        else
+        {
+            if(dist < distanceToPOI)
+            {
+                target = poi;
+                distanceToPOI = dist;
+            }
+        }
+    }
+}
---
+++ Assets/POI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class POI : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Collector.s_myCollector != null)
+        {
+            Collector.s_myCollector.addPOI(this);
+        }
+	
+	}
+}
