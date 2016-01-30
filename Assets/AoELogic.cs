5a6e8f0562300d60c4fbcea9327704ca8c7784a6
Leonid Sinyakov <redeyerulk@gmail.com>
2016-01-30 15:33:19 +0300

    working AoE

---
+++ Assets/AoELogic.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class AoELogic : NetworkBehaviour
+{
+    //[SyncVar]
+    public float speedMult;
+
+    //[SyncVar]
+    public float duration;
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (!isServer)
+            return;
+        var collector = collider.gameObject.GetComponent<Collector>();
+
+        if (collector != null)
+        {
+            collector.ApplySpeedChange(speedMult);
+        }
+    }
+
+    void Update()
+    {
+        if (!isServer)
+            return;
+
+        duration -= Time.deltaTime;
+        if(duration < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/UILogic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UILogic : MonoBehaviour {
+
+	public void onSlowDown()
+    {
+        Player.s_localPlayer.nextAction = Player.Action.Slow;
+    }
+     
+    public void onHaste()
+    {
+        Player.s_localPlayer.nextAction = Player.Action.Haste;
+    }
+
+    public void onDamage()
+    {
+        Player.s_localPlayer.nextAction = Player.Action.Damage;
+        
+    }
+
+    public void onCP()
+    {
+        Player.s_localPlayer.nextAction = Player.Action.CP;
+    }
+}
